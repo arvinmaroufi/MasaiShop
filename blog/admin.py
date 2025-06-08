24f576004d4fa9da1148458d5e7d50e466833178
arvinmaroufi <arvinmaroufi.dev@gmail.com>
2025-06-08 04:48:38 +0430

    Article is added to the models and migrated to the db

--- blog/admin.py
+++ blog/admin.py
@@ -13,3 +13,21 @@ class CategoryAdmin(ModelAdminJalaliMixin, admin.ModelAdmin):
     def get_created_jalali(self, obj):
         return datetime2jalali(obj.created_at).strftime('%a, %d %b %Y')
 
+
+@admin.register(models.Article)
+class ArticleAdmin(ModelAdminJalaliMixin, admin.ModelAdmin):
+    list_display = ['short_title', 'author', 'views', 'article_image', 'get_created_jalali', 'status']
+    prepopulated_fields = {'slug': ('title',)}
+    list_editable = ['status']
+
+    def short_title(self, obj):
+        if len(obj.title) > 20:
+            return obj.title[:20] + '...'
+        return obj.title
+    short_title.short_description = 'عنوان مقاله'
+
+    @admin.display(description='تاریخ ایجاد', ordering='created_at')
+    def get_created_jalali(self, obj):
+        return datetime2jalali(obj.created_at).strftime('%a, %d %b %Y')
+
+
--- blog/models.py
+++ blog/models.py
@@ -1,4 +1,13 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.utils.html import format_html
+from ckeditor_uploader.fields import RichTextUploadingField
+
+
+STATUS = (
+    ('draft', 'پیش نویس'),
+    ('published', 'منتشر شود'),
+)
 
 
 class Category(models.Model):
@@ -9,6 +18,31 @@ class Category(models.Model):
     class Meta:
         verbose_name = 'دسته بندی'
         verbose_name_plural = 'دسته بندی ها'
+
+    def __str__(self):
+        return self.title
+
+
+class Article(models.Model):
+    author = models.ForeignKey(User, on_delete=models.CASCADE, verbose_name='نویسنده مقاله')
+    category = models.ManyToManyField(Category, related_name='articles', verbose_name='دسته بندی مربوطه')
+    title = models.CharField(max_length=200, unique=True, verbose_name='عنوان مقاله')
+    slug = models.SlugField(max_length=200, unique=True, allow_unicode=True, verbose_name='نامک')
+    description = RichTextUploadingField(verbose_name='متن مقاله')
+    image = models.ImageField(upload_to='articles/', blank=True, null=True, verbose_name='تصویر مقاله')
+    status = models.CharField(choices=STATUS, max_length=10, verbose_name='وضعیت')
+    views = models.IntegerField(default=0, editable=False, verbose_name='بازدید')
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name='تاریخ ایجاد')
+    updated_at = models.DateTimeField(auto_now=True, verbose_name='تاریخ بروزرسانی')
+
+    class Meta:
+        verbose_name = 'مقاله'
+        verbose_name_plural = 'مقالات'
+
+    def article_image(self):
+        if self.image:
+            return format_html(f'<img src="{self.image.url}" width="50px" height="50px">')
+        return format_html(f'<h3 style="color: red">تصویر ندارد</h3>')
 
     def __str__(self):
         return self.title
