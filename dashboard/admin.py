ee3d5cc2f21921379807509bd36b8b36aaf52928
arvinmaroufi <arvinmaroufi.dev@gmail.com>
2025-06-15 05:06:45 +0430

    Address is added to the models and migrated to the db

--- dashboard/admin.py
+++ dashboard/admin.py
@@ -1,3 +1,25 @@
 from django.contrib import admin
+from . import models
+from jalali_date import datetime2jalali, date2jalali
+from jalali_date.admin import ModelAdminJalaliMixin
 
-# Register your models here.
+
+@admin.register(models.Address)
+class AddressAdmin(ModelAdminJalaliMixin, admin.ModelAdmin):
+    list_display = ['user', 'title', 'short_full_address', 'city', 'province', 'postal_code', 'receiver_name',
+                    'phone_number', 'is_default', 'get_created_at_jalali', 'get_updated_at_jalali']
+    list_editable = ['is_default']
+
+    def short_full_address(self, obj):
+        if len(obj.full_address) > 20:
+            return obj.full_address[:20] + '...'
+        return obj.full_address
+    short_full_address.short_description = 'آدرس کامل'
+
+    @admin.display(description='تاریخ ایجاد', ordering='created_at')
+    def get_created_at_jalali(self, obj):
+        return datetime2jalali(obj.created_at).strftime('%a, %d %b %Y')
+
+    @admin.display(description='تاریخ به‌روزرسانی', ordering='updated_at')
+    def get_updated_at_jalali(self, obj):
+        return datetime2jalali(obj.updated_at).strftime('%a, %d %b %Y')
--- dashboard/models.py
+++ dashboard/models.py
@@ -1,3 +1,31 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.core.validators import RegexValidator
 
-# Create your models here.
+
+class Address(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='addresses', verbose_name='کاربر')
+    title = models.CharField(max_length=100, verbose_name='عنوان آدرس')
+    full_address = models.TextField(verbose_name='آدرس کامل')
+    city = models.CharField(max_length=50, verbose_name='شهر')
+    province = models.CharField(max_length=50, verbose_name='استان')
+    postal_code = models.CharField(max_length=10, validators=[RegexValidator(regex='^\d{10}$', message='کد پستی باید 10 رقم باشد')], verbose_name='کد پستی')
+
+    receiver_name = models.CharField(max_length=100, verbose_name='نام تحویل گیرنده')
+    phone_number = models.CharField(max_length=11, validators=[RegexValidator(regex='^09\d{9}$', message='شماره موبایل باید با 09 شروع شده و 11 رقم باشد')], verbose_name='شماره موبایل')
+
+    is_default = models.BooleanField(default=False, verbose_name='آدرس پیش‌فرض')
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name='تاریخ ایجاد')
+    updated_at = models.DateTimeField(auto_now=True, verbose_name='تاریخ به‌روزرسانی')
+
+    class Meta:
+        verbose_name = 'آدرس'
+        verbose_name_plural = 'آدرس‌ ها'
+
+    def __str__(self):
+        return f"{self.title} - {self.receiver_name} ({self.city})"
+
+    def save(self, *args, **kwargs):
+        if self.is_default:
+            self.user.addresses.exclude(pk=self.pk).update(is_default=False)
+        super().save(*args, **kwargs)
