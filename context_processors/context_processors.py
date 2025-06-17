1980e270143d9de53a10f28464245e6962c7f578
arvinmaroufi <arvinmaroufi.dev@gmail.com>
2025-06-17 10:27:36 +0430

    base.html and context_processors.py is updated

--- context_processors/context_processors.py
+++ context_processors/context_processors.py
@@ -1,5 +1,6 @@
 from core.models import SiteSettings
 from product.models import ProductCategory, ProductColor, ProductBrand
+from cart.models import Cart, CartItem
 
 
 def shop_func(request):
@@ -7,10 +8,27 @@ def shop_func(request):
     categories = ProductCategory.objects.all()
     colors = ProductColor.objects.all()
     brands = ProductBrand.objects.all()
+
+    cart = None
+    cart_items = []
+    cart_total = 0
+    if request.user.is_authenticated:
+        try:
+            cart = Cart.objects.filter(user=request.user).first()
+            if cart:
+                cart_items = CartItem.objects.filter(cart=cart).select_related('product')
+                cart_total = cart.total_price
+        except:
+            pass
+
     return {
         'site_settings': site_settings,
         'categories': categories,
         'colors': colors,
         'brands': brands,
+
+        'cart': cart,
+        'cart_items': cart_items,
+        'cart_total': cart_total,
     }
 
