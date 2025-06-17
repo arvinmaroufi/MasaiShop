6ae1a4af92498e0cf1844ca860e9e33b2b5ab13f
arvinmaroufi <arvinmaroufi.dev@gmail.com>
2025-06-17 09:03:23 +0430

    cart function is added to the views.py and urls.py and cart.html to the cart app

--- cart/urls.py
+++ cart/urls.py
@@ -4,5 +4,5 @@ from . import views
 
 app_name = 'cart'
 urlpatterns = [
-
+    path('', views.cart, name='cart'),
 ]
--- cart/views.py
+++ cart/views.py
@@ -1,3 +1,34 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
+from django.contrib.auth.decorators import login_required
+from django.http import JsonResponse
+from django.contrib import messages
+from .models import Cart, CartItem, Coupon, Order
+from product.models import Product, ProductColor
+from dashboard.models import Address
+from django.utils import timezone
+import json
 
-# Create your views here.
+
+@login_required
+def cart(request):
+    cart, created = Cart.objects.get_or_create(user=request.user)
+    cart_items = cart.items.all()
+
+    if not cart_items.exists() and cart.coupon:
+        cart.coupon = None
+        cart.coupon_discount = 0
+        cart.save()
+
+    total_price = sum(item.total_price for item in cart_items) if cart_items else 0
+    shipping_cost = 50000 if total_price < 10500000 else 0
+    final_price = total_price - cart.coupon_discount + shipping_cost
+
+    context = {
+        'cart': cart,
+        'cart_items': cart_items,
+        'total_price': total_price,
+        'shipping_cost': shipping_cost,
+        'final_price': final_price,
+    }
+    return render(request, 'cart/cart.html', context)
+
