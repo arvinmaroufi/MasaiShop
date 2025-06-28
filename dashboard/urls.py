0128647ae001293f11c1558279b2022245be6ddc
arvinmaroufi <arvinmaroufi.dev@gmail.com>
2025-06-28 12:05:43 +0430

    the url for the cart_summary function has been added to the dashboard apps urls.py file

--- dashboard/urls.py
+++ dashboard/urls.py
@@ -8,4 +8,5 @@ urlpatterns = [
     path('orders/', views.order_list, name='orders'),
     path('orders/<str:order_number>/', views.order_detail, name='order_detail'),
     path('orders_delivered/', views.orders_delivered, name='orders_delivered'),
+    path('cart_summary/', views.cart_summary, name='cart_summary'),
 ]
