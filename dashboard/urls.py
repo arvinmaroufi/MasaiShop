7e0d2a6a3dac8f80d30c6bd15f5731edce8ec97a
arvinmaroufi <arvinmaroufi.dev@gmail.com>
2025-06-30 05:15:30 +0430

    the url for the notifications_list function has been added to the dashboard apps urls.py file

--- dashboard/urls.py
+++ dashboard/urls.py
@@ -15,4 +15,5 @@ urlpatterns = [
     path('wishlist/add/<int:product_id>/', views.add_to_wishlist, name='add_to_wishlist'),
     path('wishlist/remove/<int:product_id>/', views.remove_from_wishlist, name='remove_from_wishlist'),
     path('addresses/', views.user_addresses, name='user_addresses'),
+    path('notifications/', views.notifications_list, name='notifications'),
 ]
